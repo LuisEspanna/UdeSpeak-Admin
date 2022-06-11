0fda19251e4430472ce285ea31ae74af8b5e33e1
Luis Espana <fdh.luis.angel@gmail.com>
2022-06-10 19:00:29 -0500

    :construction: Header created

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './button.scss';
+
+export default function Button({className, value, onClick, children}) {
+  return (
+    <button
+        className={`button-primary ${className && className}`}
+        onClick={onClick}
+    >
+        {
+            children ? children : value
+        }
+    </button>
+  )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../Button/Button'
+import './header.scss'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <Button>
+            Hola
+        </Button>
+    </div>
+  )
+}
