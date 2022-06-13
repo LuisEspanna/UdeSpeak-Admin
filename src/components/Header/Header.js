5858ed8e6978c8e308e2c871bef355a0d750e036
Luis Espana <fdh.luis.angel@gmail.com>
2022-06-13 18:09:55 -0500

    :construction: sidebar actions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,27 @@ import React from 'react'
 import Button from '../Button/Button'
 import HamburguerIcon from '../icons/HamburguerIcon'
 import './header.scss'
+import { useDispatch } from 'react-redux'
+import { click } 
+from '../../state/reducers/sidebarSlice'
 
 export default function Header() {
-  return (
-    <div className='header d-flex'>
-        <Button className='me-3'>
-            <HamburguerIcon/>
-        </Button>
-        <Button className='me-auto'>
-            <HamburguerIcon/>
-        </Button>
-        <Button className='mx-3'>
-            <HamburguerIcon/>
-        </Button>
-        <Button className=''>
-            <HamburguerIcon/>
-        </Button>
-    </div>
-  )
+    const dispatch = useDispatch()
+    
+    return (
+        <div className='header d-flex'>
+            <Button className='me-3' onClick={() => dispatch(click())}>
+                <HamburguerIcon />
+            </Button>
+            <Button className='me-auto'>
+                <HamburguerIcon />
+            </Button>
+            <Button className='mx-3'>
+                <HamburguerIcon />
+            </Button>
+            <Button className=''>
+                <HamburguerIcon />
+            </Button>
+        </div>
+    )
 }
--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './reducers/userSlice'
+import sidebarSlice from './reducers/sidebarSlice'
 
 export default configureStore({
     reducer: {
-        user: userSlice
+        user: userSlice,
+        sidebar: sidebarSlice
     },
 });
