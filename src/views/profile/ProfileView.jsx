255f7c7b72bc2e50afd3c95443ed1f5346e055fb
Luis Espana <fdh.luis.angel@gmail.com>
2023-02-05 15:39:10 -0500

    :construction: Work in progress :: Drawer alerts and info

---
+++ src/views/profile/ProfileView.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react';
+import Card from '../../components/card/Card';
+import TextField from '../../components/form/textField/TextField';
+import useProfileView from './hooks/useProfileView';
+import Avatar from '../../components/avatar/Avatar';
+import './styles.scss';
+import { useSelector } from 'react-redux';
+import { getDisplayName } from '../../functions';
+import Button from '../../components/button/Button';
+
+
+export default function ProfileView() {
+  const ref = useRef();
+  const user = useSelector(state => state.user);
+  const {handleDeleteAccount} = useProfileView();
+
+  return (
+    <div ref={ref}>
+      <Card className='profile-view'>
+
+        <span className='view-title'>
+          Mi Cuenta
+        </span>
+
+        <div className='user-card my-4 row p-4'>
+          <div className='col-2'>
+            <Avatar photoURL={user.photoURL}/>
+          </div>
+          <div className='col'>
+            <div className='my-2 fs1'><b>{getDisplayName(user.displayName)}</b></div>
+            <div className='my-2'>{user.email}</div>
+            <div>
+              <span>Permisos: </span> <span>{user.permission}</span>
+            </div>         
+          </div>
+        </div>
+
+        <span className='fs2 fbold mt-4'>
+          Información general
+        </span>
+
+        <div className='cform'>
+          <TextField className='mt-2' placeholder='Nombre completo'  name='name' value={user.displayName}/>
+          <TextField className='mt-3' placeholder='Correo electrónico'  name='name' value={user.email}/>
+        </div>
+
+        <br/>
+        <Button type='danger' title='Eliminar cuenta' className='px-2 my-2' onClick={handleDeleteAccount}/>
+      </Card>
+    </div>
+  )
+}
--- src/views/profile/hooks/useProfileView.js
+++ src/views/profile/hooks/useProfileView.js
@@ -1,5 +1,31 @@
-export default function useUsersView(ref) {  
+import Swal from "sweetalert2";
+
+export default function useUsersView(ref) {
+    //TODO: conectar con DB
+    const handleDeleteAccount = () => {
+        Swal.fire({
+            title: 'Está seguro que desea eliminar su cuenta?',
+            text: "Esta acción no se puede revertir",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Eliminada!',
+                    'Tu prtfil ha sido eliminado',
+                    'success'
+                );
+
+                console.log('Eliminando');
+            }
+        });
+    }
 
     return {
+        handleDeleteAccount
     }
 }
