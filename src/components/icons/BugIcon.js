195db2f80ca1c1adc575c87850ed6e3e0b0038a6
Luis Espana <fdh.luis.angel@gmail.com>
2023-09-09 22:16:10 -0500

    :zap: General update :: Menu options

---
+++ src/components/icons/BugIcon.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function TrashIcon(props) {
+    return (
+        <svg {...props} viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_2780_422)" fill="none">
+                <path d="M16.875 16.875V15C16.875 13.5082 17.4676 12.0774 18.5225 11.0225C19.5774 9.96763 21.0082 9.375 22.5 9.375C23.9918 9.375 25.4226 9.96763 26.4775 11.0225C27.5324 12.0774 28.125 13.5082 28.125 15V16.875"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M15 16.875H30C31.1168 18.5486 31.7642 20.4911 31.875 22.5V28.125C31.875 30.6114 30.8873 32.996 29.1291 34.7541C27.371 36.5123 24.9864 37.5 22.5 37.5C20.0136 37.5 17.629 36.5123 15.8709 34.7541C14.1127 32.996 13.125 30.6114 13.125 28.125V22.5C13.2358 20.4911 13.8832 18.5486 15 16.875Z"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M5.625 24.375H13.125"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M31.875 24.375H39.375"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M22.5 37.5V26.25"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M7.5 35.625L13.7812 31.875"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M37.5 35.625L31.2188 31.875"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M7.5 13.125L14.5312 17.625"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M37.5 13.125L30.4688 17.625"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_2780_422">
+                    <rect width="45" height="45" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+
+    )
+}
---
+++ src/components/icons/LanguageIcon.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function TrashIcon(props) {
+    return (
+        <svg {...props} viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_2781_440)">
+                <path fill="none" d="M16.875 5.625V9.375C16.875 17.6588 12.6769 24.375 7.5 24.375"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path fill="none" d="M9.375 16.875C9.375 20.895 14.91 24.2025 21.9375 24.375"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path fill="none" d="M22.5 37.5L30 20.625L37.5 37.5"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path fill="none" d="M7.5 9.375H20.625"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+                <path fill="none" d="M35.8125 33.75H24.1875"  strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_2781_440">
+                    <rect width="45" height="45" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+
+    )
+}
